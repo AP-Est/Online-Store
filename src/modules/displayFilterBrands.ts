6d1a2ca9d56b54cd8702837a67132c7d532629ee
Andrey Piarouski <EstSkif@gmail.com>
2022-12-27 19:25:50 +0400

    fix: fix checkbox styles

--- src/modules/displayFilterBrands.ts
+++ src/modules/displayFilterBrands.ts
@@ -15,6 +15,7 @@ export default function displayFilterBrands() {
         const unreadyTitles = filterBrandsArray[i] as string;
         const brandTitle = unreadyTitles.split(' ').join('');
         const filterBrandsChBox = document.createElement('input');
+        filterBrandsChBox.classList.add('checkBoxStyle');
         filterBrandsChBox.classList.add(`filterBrandsBox__${brandTitle}`);
         filterBrandsChBox.type = 'checkbox';
         filterBrandsChBox.id = `filterBrandsBox__${brandTitle}`;
