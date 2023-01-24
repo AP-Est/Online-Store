f6d82cb1df2c64d68af79f739c4ce671c722cb1b
Andrey Piarouski <EstSkif@gmail.com>
2023-01-24 23:27:38 +0400

    fix: cardCVV NaN

--- src/components/CartPageModel.ts
+++ src/components/CartPageModel.ts
@@ -52,7 +52,7 @@ export class CartPageModel {
             cardType:
                 'https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=700&quality=85&auto=format&fit=max&s=fb1dca6cdd4589cd9ef2fc941935de71',
             cardValid: '',
-            cardCVV: NaN,
+            cardCVV: 0,
         };
         this.modalOn = localStorage.modalOn;
         this.checkModalOn();
